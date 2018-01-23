c8fc540f42589013d5d0b17c5dc037a7d7c8a754
Yos Riady <yos.riady@grabtaxi.com>
2018-01-23 18:43:24 +0800

    Work on completeTodo

--- README.md
+++ README.md
@@ -37,9 +37,26 @@ go get github.com/aws/aws-lambda-go/lambda
 
 4. Test:
 
+> Use [this Postman collection](https://www.getpostman.com/collections/4cff6cc5d40753d75348) or use cURL:
+
 ```
-> curl -X POST https://<hash>.execute-api.<region>.amazonaws.com/dev/todos
-[]
+> curl https://<hash>.execute-api.<region>.amazonaws.com/dev/todos
+{
+    "todos": [
+        {
+            "id": "d3e38e20-5e73-4e24-9390-2747cf5d19b5",
+            "description": "buy fruits",
+            "done": false,
+            "created_at": "2018-01-23 08:48:21.211887436 +0000 UTC m=+0.045616262"
+        },
+        {
+            "id": "1b580cc9-a5fa-4d29-b122-d20274537707",
+            "description": "go for a run",
+            "done": false,
+            "created_at": "2018-01-23 10:30:25.230758674 +0000 UTC m=+0.050585237"
+        }
+    ]
+}
 ```
 
 ## TODOs
--- src/handlers/completetodo.go
+++ src/handlers/completetodo.go
@@ -1,21 +1,63 @@
 package main
 
 import (
+	"fmt"
+	"context"
+	"os"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-lambda-go/events"
-	"context"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/aws"
 )
 
+var ddb *dynamodb.DynamoDB
+func init() {
+	region := os.Getenv("AWS_REGION")
+	if session, err := session.NewSession(&aws.Config{ // Use aws sdk to connect to dynamoDB
+		Region: &region,
+	}); err != nil {
+		fmt.Println(fmt.Sprintf("Failed to connect to AWS: %s", err.Error()))
+	} else {
+		ddb = dynamodb.New(session) // Create DynamoDB client
+	}
+}
+
+
 func CompleteTodo(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// Parse id from request body
+	id := request.PathParameters["id"]
 
-	// 1. Parse id from request body
-	// 2. ddb.UpdateItem to mark the row as Done: true
-	// 3. Return HTTP response
+	// TOFIX: not working
+	// Write to DynamoDB
+	tableName := aws.String(os.Getenv("TODOS_TABLE_NAME"))
+	_, err := ddb.UpdateItem(&dynamodb.UpdateItemInput{
+		// TODO: Expression attribute names
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":d": {
+				BOOL: aws.Bool(true),
+			},
+		},
+		TableName: tableName,
+		Key: map[string]*dynamodb.AttributeValue{
+			"id": {
+				S: aws.String(id),
+			},
+		},
+		UpdateExpression: aws.String("set todo.done = :d"),
+	})
 
-	return events.APIGatewayProxyResponse{
-		Body: request.Body,
-		StatusCode: 200,
-	}, nil
+	if err != nil {
+		return events.APIGatewayProxyResponse{ // Error HTTP response
+			Body: err.Error(),
+			StatusCode: 500,
+		}, nil
+	} else {
+		return events.APIGatewayProxyResponse{ // Success HTTP response
+			Body: request.Body,
+			StatusCode: 200,
+		}, nil
+	}
 }
 
 func main() {
